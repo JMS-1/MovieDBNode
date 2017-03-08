12bfadc496544a58f866f5392480705e3fc59242
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-03-08 10:49:17 +0100

    First walk through migration of first object type

--- MongoDbWriter/app.ts
+++ MongoDbWriter/app.ts
@@ -1,4 +1,28 @@
-﻿import * as legacy from "./LegacyContract";
+﻿import * as mongo from "mongodb";
+
+import * as legacy from "./LegacyContract";
+import * as model from "./model";
+
+function reportError(error: any): void {
+    console.log(error.message);
+}
 
 legacy.loadDump((error, data) => {
+    mongo
+        .MongoClient
+        .connect("mongodb://localhost:27017/test2")
+        .then(db => {
+            // Cleanup database
+            db
+                .dropCollection(model.containerCollection)
+                .then(success => success, error => null)
+                .then(success => {
+                    var containerCollection = db.collection(model.containerCollection);
+                    var containers = model.migrateContainers(data.Containers);
+
+                    return containerCollection.insertMany(containers);
+                })
+                .then(result => {
+                });
+        }, reportError);
 });
---
+++ MongoDbWriter/model.ts
@@ -0,0 +1,55 @@
+﻿import * as legacy from "./LegacyContract";
+
+export var containerCollection = "containers";
+
+export interface IContainer {
+    _id: number;
+
+    name: string;
+
+    type: number;
+
+    description?: string;
+
+    parentId?: number;
+
+    location?: string;
+}
+
+var _containerId = 0;
+
+function migrateContainer(old: legacy.IContainer): IContainer {
+    return {
+        parentId: null,
+        name: old.Name,
+        type: old.Type,
+        _id: ++_containerId,
+        location: old.ParentLocation || null,
+        description: old.Description || null
+    };
+}
+
+export function migrateContainers(old: legacy.IContainer[]): IContainer[] {
+    var containers = [];
+
+    var idMap: { [id: string]: IContainer; } = {};
+    var parentMap: { [id: string]: string; } = {};
+
+    if (old)
+        for (var oldContainer of old) {
+            var container = migrateContainer(oldContainer);
+
+            idMap[oldContainer.Id] = container;
+
+            if (oldContainer.Parent)
+                parentMap[oldContainer.Id] = oldContainer.Parent;
+
+            containers.push(container);
+        }
+
+    for (var id in parentMap)
+        if (parentMap.hasOwnProperty(id))
+            idMap[id].parentId = idMap[parentMap[id]]._id;
+
+    return containers;
+}
