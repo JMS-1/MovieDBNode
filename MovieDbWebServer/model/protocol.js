50137bc6dad51da632aef5f6637f63e812c2661f
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-03-09 07:19:05 +0100

    Minor code reorg

--- MovieDbWebServer/model/protocol.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=protocol.js.map
--- MovieDbWebServer/routes/app.ts
+++ MovieDbWebServer/routes/app.ts
@@ -1,11 +1,24 @@
 ﻿import * as express from 'express';
 
-import * as model from '../model/app';
+import * as rest from './protocol';
+
+function getInfo(): rest.ApplicationInformation {
+    return {
+        total: 0,
+        genres: [],
+        series: [],
+        empty: false,
+        languages: [],
+        containers: [],
+        seriesSeparator: ">",
+        urlExpression: "https?:\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-\\/]))?"
+    };
+}
 
 const router = express.Router();
 
 router.get('/info', (req: express.Request, res: express.Response) => {
-    res.json(model.getInfo());
+    res.json(getInfo());
 });
 
 export default router;
