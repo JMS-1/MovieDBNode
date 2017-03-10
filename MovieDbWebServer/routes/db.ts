7a7cb5a02b2adbe2bac2a15324f938929ffa766b
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-03-10 23:15:36 +0100

    Started query

--- MovieDbWebServer/routes/db.ts
+++ MovieDbWebServer/routes/db.ts
@@ -1,8 +1,29 @@
 ﻿import { Router, Request, Response } from 'express';
+import { Collection } from 'mongodb';
 
-import { ISearchRequest, ISearchInformation, sendJson } from './protocol';
+import { ISearchRequest, ISearchInformation, ILanguageSearchInformation, IGenreSearchInformation, sendJson } from './protocol';
+import { recordingCollection } from '../database/model';
+import { connect } from '../database/db';
 
 const router = Router();
+
+interface IGroup {
+    group: number;
+
+    count: number;
+}
+
+function getCounts(recordings: Collection, query: any, field: string): Promise<IGroup[]> {
+    return recordings
+        .aggregate<IGroup>([
+            { $match: query },
+            { $project: { _id: 0, [field]: 1 } },
+            { $unwind: `$${field}` },
+            { $group: { _id: `$${field}`, count: { $sum: 1 } } },
+            { $project: { _id: 0, group: "$_id", count: 1 } }
+        ])
+        .toArray() as Promise<IGroup[]>;
+}
 
 function query(request?: ISearchRequest): Promise<ISearchInformation> {
     if (!request)
@@ -15,15 +36,24 @@ function query(request?: ISearchRequest): Promise<ISearchInformation> {
             page: 0,
         };
 
-    return new Promise<ISearchInformation>(setResult => {
-        setResult({
-            page: 0,
-            total: 0,
-            genres: [],
-            languages: [],
-            recordings: [],
-            size: request.size
-        });
+    return connect().then(db => {
+        var fields = { name: 1, rentTo: 1, genres: 1, series: 1, created: 1, languages: 1 };
+        var query = {};
+
+        var rec = db.collection(recordingCollection);
+
+        return rec.count(query)
+            .then(total => rec.find(query, fields, request.page * request.size, request.size).toArray()
+                .then(allRecordings => getCounts(rec, query, "languages")
+                    .then(allLanguages => getCounts(rec, query, "genres")
+                        .then(allGenres => <ISearchInformation>{
+                            total: total,
+                            recordings: [],
+                            page: request.page,
+                            size: request.size,
+                            genres: allGenres.map(l => <IGenreSearchInformation>{ id: `${l.group}`, count: l.count }),
+                            languages: allLanguages.map(l => <ILanguageSearchInformation>{ id: `${l.group}`, count: l.count })
+                        }))));
     });
 }
 
