25659c0d5a5194d417ed064a49813b760e367da7
Dr. Jochen Manns <manns@insynergie.de>
2019-06-23 16:15:21 +0200

    added server tsconfig

--- Server/build/startup.js
+++ Server/build/startup.js
@@ -1,8 +1,8 @@
 "use strict";
-exports.__esModule = true;
-var express = require("express");
-var path_1 = require("path");
-var app = express();
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const path_1 = require("path");
+const app = express();
 app.use(express.static(path_1.join(__dirname, '../dist')));
 app.listen(29099);
 
