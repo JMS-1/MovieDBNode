117e200f1ab3b628b689141b9a443be29114d872
Dr. Jochen Manns <manns@insynergie.de>
2019-07-03 10:30:46 +0200

    remove build targets from GIT

--- Server/src/api/container.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("./utils");
-const container_1 = require("../database/container");
-exports.containerApi = utils_1.createApiRouter('/container', container_1.containerCollection, container_1.toProtocol, container_1.toEntity);
-
-//# sourceMappingURL=container.js.map
--- Server/src/api/series.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("./utils");
-const series_1 = require("../database/series");
-exports.seriesApi = utils_1.createApiRouter('/series', series_1.seriesCollection, series_1.toProtocol, series_1.toEntity);
-
-//# sourceMappingURL=series.js.map
