03091e908498edbcda786f85028e8f46e5a55f6f
Dr. Jochen Manns <manns@insynergie.de>
2020-04-12 12:42:11 +0200

    minor

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const path = require('path')
 
 module.exports = () => {
     return {
+        devtool: 'source-map',
         entry: { index: './Client/src/index.tsx' },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js'],
-        },
-        output: {
-            path: path.join(__dirname, './Server/dist'),
-            filename: '[name].js',
-        },
-        optimization: {
-            minimize: false,
-        },
         module: {
             rules: [
                 {
-                    test: /\.tsx?$/,
                     loader: 'awesome-typescript-loader',
                     options: {
                         configFileName: 'Client/tsconfig.json',
                     },
+                    test: /\.tsx?$/,
                 },
             ],
         },
-        devtool: 'source-map',
+        optimization: {
+            minimize: false,
+        },
+        output: {
+            filename: '[name].js',
+            path: path.join(__dirname, './Server/dist'),
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
     }
 }
