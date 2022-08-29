990b4c5d06804ae4235119783d11832c41fe6f08
Dr. Jochen Manns <manns@insynergie.de>
2022-08-29 20:48:51 +0200

    feat: Schnittstellenbeschreibung vervollständigt.

--- Client/src/types.d.ts
+++ Client/src/types.d.ts
@@ -3,7 +3,27 @@ declare module 'api' {
 
     interface ILanguage {}
 
-    interface IRecording {}
+    interface ILink {
+        description?: string
+        name: string
+        url: string
+    }
+
+    interface IRecording {
+        _id: string
+        containerId?: string
+        containerPosition?: string
+        containerType: 'BluRay' | 'DVD' | 'RecordedDVD' | 'SuperVideoCD' | 'Undefined' | 'VideoCD'
+        created: string
+        description?: string
+        fullName: string
+        genres: string[]
+        languages: string[]
+        links: ILink[]
+        name: string
+        rentTo?: string
+        series?: string
+    }
 
     interface IRecordingQueryRequest {
         correlationId?: string
