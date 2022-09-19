6b461302b14a4713c2c7a6082f7069e44c634fe0
Dr. Jochen Manns <manns@insynergie.de>
2022-09-19 13:37:20 +0200

    chore: Deploymentartefakte reduziert.

--- deploy/src/model/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uniqueIdPattern = void 0;
-const hex = '[0-9a-fA-F]';
-exports.uniqueIdPattern = `^${hex}{8}-(${hex}{4}-){3}${hex}{12}$`;
-//# sourceMappingURL=utils.js.map
