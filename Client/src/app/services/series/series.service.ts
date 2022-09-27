4d29ade4b4d2d99e49bae776186914be1cb42b5f
Dr. Jochen Manns <manns@insynergie.de>
2022-09-27 12:25:20 +0200

    feat: Serien Dienst vorbereitet für Pflege der Serien.

--- Client/src/app/services/series/series.service.ts
+++ Client/src/app/services/series/series.service.ts
@@ -1,90 +1,85 @@
-import { Injectable, OnDestroy } from '@angular/core'
-import { Apollo, gql } from 'apollo-angular'
-import { EmptyObject } from 'apollo-angular/types'
+import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
+import { Apollo } from 'apollo-angular'
 import { BehaviorSubject, Observable } from 'rxjs'
 
-import { ISeries, ISeriesFindResult } from '../../../api'
+import { ISeries } from '../../../api'
+import { EditableService } from '../edit.service'
 import { ErrorService } from '../error/error.service'
-
-const querySeries = gql<{ series: { find: ISeriesFindResult } }, EmptyObject>(`
-  query {
-    series {
-        find(page: 1, pageSize: 1000) {
-          items { _id name description parentId fullName }
-        }
-      }
-    }
-`)
+import { ValidationService } from '../validation/validation.service'
 
 export interface ISeriesNode extends ISeries {
     allChildren: string[]
     children: ISeriesNode[]
+    level: number
     parent?: ISeriesNode
 }
 
-function fillChildren(series: ISeriesNode): void {
-    for (const child of series.children) {
-        series.allChildren.push(child._id)
+function setLevel(series: ISeriesNode, level = 0, all: string[] = []): void {
+    all.push(series._id)
 
-        fillChildren(child)
+    series.level = level++
 
-        series.allChildren.push(...child.allChildren)
-    }
+    series.children.forEach((c) => setLevel(c, level, series.allChildren))
+
+    all.push(...series.children.map((c) => c._id))
 }
 
 @Injectable()
-export class SeriesService implements OnDestroy {
-    private readonly _query = new BehaviorSubject<Record<string, ISeriesNode>>({})
+export class SeriesService extends EditableService<ISeriesNode> {
+    protected override readonly ignoredFields: string[] = [
+        '__typename',
+        '_id',
+        'allChildren',
+        'children',
+        'level',
+        'parent',
+    ]
 
     private readonly _roots = new BehaviorSubject<ISeriesNode[]>([])
-
-    get map(): Observable<Record<string, ISeriesNode>> {
-        return this._query
-    }
 
     get roots(): Observable<ISeriesNode[]> {
         return this._roots
     }
 
-    constructor(private readonly _gql: Apollo, private readonly _errors: ErrorService) {
-        this._errors.serverCall(this._gql.query({ query: querySeries }), (data) => {
-            const map = data.series.find.items.reduce((map, series) => {
-                if (!series.name) {
-                    series.name = series._id
-                }
+    constructor(gql: Apollo, validation: ValidationService, router: Router, errors: ErrorService) {
+        super(gql, 'Series', 'series', '_id name description parentId fullName', validation, router, errors)
 
-                if (!series.fullName) {
-                    series.fullName = series.name
-                }
-
-                map[series._id] = { ...series, allChildren: [], children: [], parent: undefined }
-
-                return map
-            }, {} as Record<string, ISeriesNode>)
-
-            const roots: ISeriesNode[] = []
-
-            for (const id of Object.keys(map)) {
-                const series = map[id]
-
-                series.parent = series.parentId ? map[series.parentId] : undefined
-
-                if (series.parent) {
-                    series.parent.children.push(series)
-                } else {
-                    roots.push(series)
-                }
-            }
-
-            roots.forEach(fillChildren)
-
-            this._query.next(map)
-            this._roots.next(roots)
-        })
+        this.load()
     }
 
-    ngOnDestroy(): void {
-        this._query.complete()
+    protected override createMap(series: ISeriesNode[]): Record<string, ISeriesNode> {
+        const map = super.createMap(series)
+
+        const roots: ISeriesNode[] = []
+
+        series.forEach((series) => {
+            series.name = series.name || series._id
+            series.fullName = series.fullName || series.name
+
+            series.allChildren = []
+            series.children = []
+            series.parent = series.parentId ? map[series.parentId] : undefined
+        })
+
+        series.forEach((series) => {
+            if (series.parent) {
+                series.parent.children.push(series)
+            } else {
+                roots.push(series)
+            }
+        })
+
+        roots.forEach((s) => setLevel(s))
+
+        this._roots.next(roots)
+
+        return map
+    }
+
+    override ngOnDestroy(): void {
+        super.ngOnDestroy()
+
         this._roots.complete()
     }
 }
